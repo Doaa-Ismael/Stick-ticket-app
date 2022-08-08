39f23b35ba9fd11cd9eb09b5d4e4484a5a254a21
Doaa-Ismael <doaaismael@outlook.com>
2022-08-08 22:12:43 +0200

    add stocks view

--- src/views/StocksView.vue
+++ src/views/StocksView.vue
@@ -1,15 +1,68 @@
 <template>
   <div class="stocks">
-    <h1>This is stocks page</h1>
+    <div class="stock" v-for="{name, open, close, high, low} in stocks">
+      <div class="details">
+        <h4  :style="{ color: close >= open ? 'green' : 'red' }">{{ close }}</h4>
+        <h4>{{ name }}</h4>
+      </div>
+      <div>
+        <p>Open: {{open}}</p>
+        <p>Close: {{close}}</p>
+        <p>High: {{high}}</p>
+        <p>Low: {{low}}</p>
+      </div>
+    </div>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      stocks: [
+          { name: "IBM", open: 89.9, close: 90, high: 100, low: 80 },
+          { name: "Aple", open: 88.9, close: 70, high: 130, low: 80 },
+          { name: "ABC", open: 89.9, close: 100, high: 100, low: 80 },
+          { name: "CBC", open: 89.9, close: 95, high: 120, low: 80 },
+          { name: "CDN", open: 89.9, close: 80, high: 97, low: 80 },
+          { name: "OPP", open: 89.9, close: 87, high: 87, low: 80 },
+      ],
+    };
+  },
+};
+</script>
+
 <style>
-@media (min-width: 1024px) {
-  .stocks {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+
+
+.stocks {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  column-gap: 8px;
+  row-gap: 8px;
+  margin-top: 8px;
 }
+
+.stock {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  min-width: 220px;
+  background: #c6c4c5b3;
+  border-radius: 10px;
+  padding: 8px;
+}
+
+.details {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+.details h4 {
+  font-size: x-large;
+}
+
+
 </style>
