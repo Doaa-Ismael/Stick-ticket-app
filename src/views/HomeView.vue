65a48027eb44e892170a27856ee260d874f65e10
Doaa-Ismael <doaaismael@outlook.com>
2022-08-08 21:21:05 +0200

    initialize the project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <h1>Stock Ticker App</h1>
+  </main>
+</template>
---
+++ src/views/StocksView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="stocks">
+    <h1>This is stocks page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .stocks {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
